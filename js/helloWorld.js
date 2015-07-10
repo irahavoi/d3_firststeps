a40e16a63f1da6b7dcc38dcbfe3863e7a0d2a1b0
unknown <irahavoi@gmail.com>
2015-07-09 21:36:38 -0500

    svg snowman

--- js/helloWorld.js
+++ js/helloWorld.js
@@ -1,4 +1,8 @@
 /**
  * Created by irahavoi on 7/9/2015.
  */
-d3.selectAll("div").style("background", "#bababa");
+d3.select("#someDiv").style("border", "5px darkgray dashed");
+d3.select("#someDiv").attr("id", "newID");
+d3.select("#someCheckbox").property("checked", true);
+d3.selectAll("svg").style("background", "black");
+d3.selectAll(".body").style("fill", "white");
